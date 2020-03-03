bb0e227c520e9bcb2f12d3884595f25376dc25ba
Steve Phillips <sjphilli@uwaterloo.ca>
2020-03-02 21:30:41 -0800

    Add some roben results for uptown garage

--- content.js
+++ content.js
@@ -7,6 +7,7 @@ let settings = {
 		{"thumb": "potree/results/results_ig/Conestogo_Bridge_Delams/conestogo_bridge_delams.png", "label": "Conestogo Bridge Delams", "url": "Conestogo_Bridge_Delams/conestogo_bridge_delams.html"},
 	],
 	"results_roben": [
+		{"thumb": "potree/results/results_roben/uptown_garage/roben_uptown_garage.png", "label": "Uptown Parking Garage", "url": "uptown_garage/roben_uptown_garage.html"},
 		{"thumb": "potree/results/results_roben/uw_garage2/roben_uw_garage2.png", "label": "UW Parking Garage (1/2)", "url": "uw_garage2/roben_uw_garage2.html"},
 		{"thumb": "potree/results/results_roben/uw_garage/roben_uw_garage.png", "label": "UW Parking Garage (2/2)", "url": "uw_garage/roben_uw_garage.html"},
 		{"thumb": "potree/results/results_roben/market_square/roben_market_square.png", "label": "Market Square Parking", "url": "market_square/roben_market_square.html"},
